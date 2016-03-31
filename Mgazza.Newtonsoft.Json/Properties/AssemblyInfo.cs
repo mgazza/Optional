d88c3e06ba44bd70bc021a7c2fd7a6b03c700487
Mark Gascoyne <mark.gascoyne@t-mac.co.uk>
2016-03-31 09:31:07 +0100

    Renamed assembly - typo :(

--- Mgazza.Newtonsoft.Json/Properties/AssemblyInfo.cs
+++ Mgazza.Newtonsoft.Json/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mgazza.Netwonsoft.Json")]
+[assembly: AssemblyTitle("Mgazza.Newtonsoft.Json")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mgazza.Netwonsoft.Json")]
+[assembly: AssemblyProduct("Mgazza.Newtonsoft.Json")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
